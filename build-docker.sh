7fd32df072072aa20fc84e2796bc1c66edf71869
Pontus <lmartins@pontusnetworks.com>
2020-01-07 18:33:16 +0000

    fixed json syntax error

--- build-docker.sh
+++ build-docker.sh
@@ -3,6 +3,9 @@
 set -e 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $DIR/
+for i in *.json; do 
+  cat $i | jq
+done
 docker build --no-cache --rm . -t pontusvisiongdpr/pontus-i18n
 
 docker push pontusvisiongdpr/pontus-i18n
